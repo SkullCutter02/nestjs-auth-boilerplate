d672989aba887b6d4b059eb1f6e373407fc5f9cf
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-06-29 19:40:06 +0800

    Reverted to non-session

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,11 +6,10 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { UserRepository } from "./user.repository";
 import { LocalStrategy } from "./local.strategy";
-import { SessionSerializer } from "./session.serializer";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository]), PassportModule.register({ session: true })],
+  imports: [TypeOrmModule.forFeature([UserRepository]), PassportModule],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, SessionSerializer],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { NestFactory } from "@nestjs/core";
-import session from "express-session";
-import passport from "passport";
 
 import { AppModule } from "./app.module";
 
@@ -8,19 +6,6 @@ const PORT = 5000 || process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // TODO: make session storage (redis or postgres)
-  app.use(
-    session({
-      secret: "secret", // TODO: move to env file
-      resave: false,
-      saveUninitialized: false,
-      cookie: { maxAge: 3_600_000 },
-    }),
-  );
-
-  app.use(passport.initialize());
-  app.use(passport.session());
 
   await app.listen(PORT);
 
